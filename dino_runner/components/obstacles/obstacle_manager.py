65ff02ecc8050866f2d330583e10ab2122782abd
Duwan Philip Parra <duwanphiliip12345@gmail.com>
2022-11-09 12:07:43 -0500

    cactus agreed

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,9 +1,11 @@
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
+import random
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
+        self.obstacles2=[]
 
     def update(self, game):
         if len(self.obstacles) == 0:
@@ -12,6 +14,18 @@ class ObstacleManager:
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                game.playing = False
+                break
+
+        if len(self.obstacles) == 0:
+            if random.randint(0,2)== 0:
+                cactus2 = Cactus(LARGE_CACTUS)
+                self.obstacles.append(cactus2)
+                obstacle.rect.y = 325
+
+        for obstacle in self.obstacles2:
+            obstacle.update(game.game_speed, self.obstacles2)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 game.playing = False
                 break
